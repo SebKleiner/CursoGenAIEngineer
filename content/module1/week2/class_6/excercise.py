a4fd0515cf9978f68ebe4af0dab63e9ca7c4c449
nicolasgaler <nicolasgaler@gmail.com>
2025-02-11 15:31:35 -0300

    Class 1

---
+++ content/module1/week2/class_6/excercise.py
@@ -0,0 +1,4 @@
+# Encadenamiento Iterativo: “Refinador de Gramática”
+# Objetivo
+# Tomar un texto que pueda tener errores gramaticales o de ortografía.
+# Seguir iterando hasta que el texto pase una verificación de “sin errores encontrados”.
